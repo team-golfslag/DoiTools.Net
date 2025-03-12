d5eb6897f0c2b8b9b66b30783ec18ba1f5d39339
Ben Stokmans <benstokmans@hotmail.nl>
2025-03-12 11:18:17 +0100

    feat: add json converter

---
+++ DoiTools.Net/DoiConverter.cs
@@ -0,0 +1,24 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DoiTools.Net;
+
+public class DoiConverter : JsonConverter<Doi>
+{
+    public override Doi Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        string? doiString = reader.GetString();
+        if (doiString is null) 
+            throw new JsonException("Expected a string value.");
+        try
+        {
+            return Doi.Parse(doiString);
+        } catch (ArgumentException e)
+        {
+            throw new JsonException("Could not parse DOI", e);
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, Doi value, JsonSerializerOptions options) => 
+        writer.WriteStringValue(value.ToString());
+}
