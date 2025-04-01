6acd282163f28c37b4b8a90287ff3decd2dd884f
Ben Stokmans <benstokmans@hotmail.nl>
2025-04-01 11:25:55 +0200

    chore: add copyright headers

--- DoiTools.Net/Doi.cs
+++ DoiTools.Net/Doi.cs
@@ -1,24 +1,32 @@
+// This program has been developed by students from the bachelor Computer Science at Utrecht
+// University within the Software Project course.
+// 
+// © Copyright Utrecht University (Department of Information and Computing Sciences)
+
 using System.Text.RegularExpressions;
 
 namespace DoiTools.Net;
 
 public partial class Doi
 {
-    private string _doi;
-    
-    private Doi(string doi) => _doi = doi;
-    
+    private readonly string _doi;
+
+    private Doi(string doi)
+    {
+        _doi = doi;
+    }
+
     [GeneratedRegex(@"^10.\d{4,9}\/[-._;()\/:A-Z0-9<>]+$", RegexOptions.IgnoreCase, "en-US")]
     private static partial Regex ModernDoiRegex();
-    
+
     [GeneratedRegex(@"^10.1002/[^\s]+$", RegexOptions.IgnoreCase, "en-US")]
     private static partial Regex OldDoiRegex();
-    
+
     /// <summary>
-    /// Implicitly convert a <see cref="string"/> to a <see cref="Doi"/> object.
+    /// Implicitly convert a <see cref="string" /> to a <see cref="Doi" /> object.
     /// </summary>
     /// <param name="doi">The DOI string to convert.</param>
-    /// <param name="result">The <see cref="Doi"/> object.</param>
+    /// <param name="result">The <see cref="Doi" /> object.</param>
     /// <returns>True if the conversion was successful, otherwise false.</returns>
     public static bool TryParse(string doi, out Doi? result)
     {
@@ -28,20 +36,20 @@ public partial class Doi
             result = new(doi);
             return true;
         }
-        
+
         result = null;
         return false;
     }
-    
+
     /// <summary>
-    /// Parse a DOI string into a <see cref="Doi"/> object.
+    /// Parse a DOI string into a <see cref="Doi" /> object.
     /// </summary>
     /// <param name="doi">The DOI string to parse.</param>
     /// <returns></returns>
     /// <exception cref="ArgumentException">Thrown when the DOI is invalid.</exception>
     public static Doi Parse(string doi)
     {
-        if (TryParse(doi, out Doi? result)) 
+        if (TryParse(doi, out Doi? result))
             return result!;
 
         throw new ArgumentException("Invalid DOI", nameof(doi));
@@ -55,9 +63,9 @@ public partial class Doi
     /// <exception cref="ArgumentException">Thrown when the DOI is invalid.</exception>
     public static string Normalize(string doi)
     {
-        if (!doi.StartsWith("http://") && !doi.StartsWith("https://")) 
+        if (!doi.StartsWith("http://") && !doi.StartsWith("https://"))
             return doi;
-        
+
         Uri url;
         try
         {
@@ -67,19 +75,19 @@ public partial class Doi
         {
             throw new ArgumentException("Invalid URL", nameof(doi));
         }
+
         if (!url.Host.StartsWith("doi.org"))
             throw new ArgumentException("Missing doi.org from URL", nameof(doi));
-            
+
         return url.AbsolutePath.Trim('/');
     }
-    
+
     /// <summary>
     /// Check if a DOI string is valid.
     /// </summary>
     /// <param name="doi">The DOI string to check.</param>
     /// <returns>True if the DOI is valid, otherwise false.</returns>
-    public static bool IsValid(string doi) => 
-        ModernDoiRegex().IsMatch(doi) || OldDoiRegex().IsMatch(doi);
-    
+    public static bool IsValid(string doi) => ModernDoiRegex().IsMatch(doi) || OldDoiRegex().IsMatch(doi);
+
     public override string ToString() => _doi;
 }
--- DoiTools.Net/DoiConverter.cs
+++ DoiTools.Net/DoiConverter.cs
@@ -1,3 +1,8 @@
+// This program has been developed by students from the bachelor Computer Science at Utrecht
+// University within the Software Project course.
+// 
+// © Copyright Utrecht University (Department of Information and Computing Sciences)
+
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -8,17 +13,18 @@ public class DoiConverter : JsonConverter<Doi>
     public override Doi Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         string? doiString = reader.GetString();
-        if (doiString is null) 
+        if (doiString is null)
             throw new JsonException("Expected a string value.");
         try
         {
             return Doi.Parse(doiString);
-        } catch (ArgumentException e)
+        }
+        catch (ArgumentException e)
         {
             throw new JsonException("Could not parse DOI", e);
         }
     }
 
-    public override void Write(Utf8JsonWriter writer, Doi value, JsonSerializerOptions options) => 
+    public override void Write(Utf8JsonWriter writer, Doi value, JsonSerializerOptions options) =>
         writer.WriteStringValue(value.ToString());
 }
